8248c249dd11bc8016f243a61ddd693e52d25fb8
Clyde Leef <chl51@georgetown.edu>
2021-02-03 17:39:59 -0500

    asked for input

--- game.py
+++ game.py
@@ -1,3 +1,16 @@
 #game.py file - this should run the rock paper scissors game
 
-print("Rock, Paper, Scissors, Shoot!")
+#title - should print player's name
+print("-------------------")
+print("Welcome 'Player One' to Rock, Paper, Scissors, Shoot!")
+print("-------------------")
+
+#asking for player's input
+choice = input("Please choose 'rock', 'paper', or 'scissors':")
+
+print("You chose: " + choice)
+
+
+
+
+exit()
