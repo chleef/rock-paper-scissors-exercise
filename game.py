016ef00c6693777af0b49f5d502af57c5930bb38
Clyde Leef <chl51@georgetown.edu>
2021-02-03 17:55:40 -0500

    simulates computer choice

--- game.py
+++ game.py
@@ -1,16 +1,21 @@
 #game.py file - this should run the rock paper scissors game
 
+import random
+
 #title - should print player's name
 print("-------------------")
 print("Welcome 'Player One' to Rock, Paper, Scissors, Shoot!")
 print("-------------------")
 
 #asking for player's input
-choice = input("Please choose 'rock', 'paper', or 'scissors':")
+player_choice = input("Please choose 'rock', 'paper', or 'scissors':")
 
-print("You chose: " + choice)
+print("You chose: " + player_choice)
 
+#simulate computer choice
+choices = ["paper", "rock", "scissors"]
+computer_choice = random.choice(choices)
 
-
+print(f"The computer chose: {computer_choice}")
 
 exit()
